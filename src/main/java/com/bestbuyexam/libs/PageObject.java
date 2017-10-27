8d6c96acf21c40b999cc66f4a0c6b3ddd85cf90c
John Warford <johnwarford@Johns-MacBook-Pro.local>
2017-10-27 10:51:22 -0700

    added page object

--- src/main/java/com/bestbuyexam/libs/PageObject.java
+++ src/main/java/com/bestbuyexam/libs/PageObject.java
@@ -1,7 +1,22 @@
 package com.bestbuyexam.libs;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 /**
  * Created by johnwarford on 2017-10-27.
  */
 public class PageObject {
+
+    protected WebDriver driver;
+    public PageObject(WebDriver driver)
+    {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public String getPageTitle()
+    {
+        return this.driver.getTitle();
+    }
 }
